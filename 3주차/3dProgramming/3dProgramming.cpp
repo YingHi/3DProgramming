45d779c6d0f40db57a0f4dbfb372b94d12a34c2d
Ogdoabyss <101399129+YingHi@users.noreply.github.com>
2022-09-18 17:43:16 +0900

    GL_POLYGON, cos, sin을 활용해 거대한 원의 꼭짓점 360개를 이어내어 큰 원을 그렸다.

---
+++ 3주차/3dProgramming/3dProgramming.cpp
@@ -0,0 +1,68 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+
+#pragma comment(lib, "OpenGL32")
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
+}
+
+
+int main(void)
+{
+    GLFWwindow* window;
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glColor3f(0.0f, 1.0f, 1.0f);
+        double rad = 1;
+        glBegin(GL_POLYGON);
+        for (int i = 0; i < 360; i++) 
+        { 
+            double angle = i * 3.141592 / 180;
+            double x = rad * cos(angle);
+            double y = rad * sin(angle);
+            glVertex2f(x, y);
+        }
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
+}
